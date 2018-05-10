e1bbafeb81be5c22103154d79649e6ddbaea334e
siniysv1 <ivan.s.sim@gmail.com>
2018-05-10 12:43:17 +0400

    Minor edit

--- misp_modules/modules/expansion/vulnerable_hosts_csv.py
+++ misp_modules/modules/expansion/vulnerable_hosts_csv.py
@@ -17,7 +17,7 @@ moduleinfo = {'version': '0.1', 'author': 'Ivan Sinyansky',
               'description': 'Getting a list of internal IPs with a vulnerability',
               'module-type': ['expansion', 'hover']}
 
-moduleconfig = ['vuln_data_file']
+moduleconfig = ['vuln_data_file', 'ip_column', 'cve_column']
 
 
 def handler(q=False):
@@ -29,17 +29,37 @@ def handler(q=False):
     if not request.get('vulnerability'):
         misperrors['error'] = 'Vulnerability ID missing for the module.'
         return misperrors
-
     vulnerability = request.get('vulnerability')
+    log.debug('Using vulnerability: {}'.format(str(vulnerability)))
 
     if not request['config'].get('vuln_data_file'):
         misperrors['error'] = 'Filename not found in config.'
         return misperrors
-
     filename = request['config'].get('vuln_data_file')
     log.debug('Using file: {}'.format(str(filename)))
 
-    ips = get_ip_by_vuln(vulnerability)
+    if not request['config'].get('ip_column'):
+        misperrors['error'] = 'ip_column not found in config.'
+        return misperrors
+    ip_column = request['config'].get('ip_column')
+    log.debug('Using ip_column: {}'.format(str(ip_column)))
+
+    if not request['config'].get('cve_column'):
+        misperrors['error'] = 'cve_column not found in config.'
+        return misperrors
+    cve_column = request['config'].get('cve_column')
+    log.debug('Using file: {}'.format(str(cve_column)))
+
+    ips = []
+    try:
+        with open(filename, 'r') as csv_file:
+            csv_data = csv.reader(csv_file, delimiter=",")
+            for row in csv_data:
+                if vulnerability in row[cve_col_num]:
+                    ips.append(row[ip_col_num])
+    except:
+        misperrors['error'] = 'Something went wrong while reading the file.'
+        return misperrors
 
     results = {'results': [{'types': mispattributes['output'],
                       'values': ips}]}
@@ -49,35 +69,6 @@ def handler(q=False):
 def introspection():
     return mispattributes
 
-
 def version():
     moduleinfo['config'] = moduleconfig
     return moduleinfo
-
-def test_vuln_data_file(request):
-    if request.get('config'):
-        config = request['config']
-        if config.get('vuln_data_file'):
-            filename = config['vuln_data_file']
-            if os.path.isfile(filename):
-                return True
-            else:
-                log.info('Cannot find specified file {}'.format(filename))
-                return False
-        else:
-            log.info('Cannot open config section in request')
-            return False
-    else:
-        return False
-
-def get_ip_by_vuln(vuln):
-    ips = ['127.0.0.1']
-    return ips
-
-def get_ip_num_by_vuln(vuln):
-    num = 0
-    return num
-
-def print_request(request):
-    log.debug('Request: {}'.format(str(request)))
-    # print(request)
